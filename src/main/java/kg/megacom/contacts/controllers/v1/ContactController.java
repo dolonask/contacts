5176252091f97f60232ee0f1635e932cc15a61a3
daskarbekov <dolon91aa@gmail.com>
2022-08-11 10:08:48 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/contacts/controllers/v1/ContactController.java
@@ -0,0 +1,23 @@
+package kg.megacom.contacts.controllers.v1;
+
+import kg.megacom.contacts.models.Contact;
+import kg.megacom.contacts.services.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/contact")
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+
+    @PostMapping("/add")
+    public Contact save(@RequestBody Contact contact){
+        return contactService.save(contact);
+    }
+}
